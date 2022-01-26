8c8943571939b02b3e043e191777bf5a08867cef
Luís Felipe <luisfelipenovais98@gmail.com>
2022-01-26 19:42:32 -0300

    Aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import appConfig from "../config.json";
+import { useRouter } from "next/router";
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import React from "react";
 
 function Titulo(props) {
   const Tag = props.tag ? props.tag : "h1";
@@ -49,11 +20,11 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = "felipeproj";
+  const [username, setUsername] = React.useState("felipeproj");
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -88,6 +59,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              roteamento.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -111,6 +86,12 @@ export default function PaginaInicial() {
 
             <TextField
               fullWidth
+              value={username}
+              onChange={(event) => {
+                const valor = event.target.value;
+                // Troca o valor da variável através do React state
+                setUsername(valor);
+              }}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -155,7 +136,9 @@ export default function PaginaInicial() {
                 borderRadius: "50%",
                 marginBottom: "16px",
               }}
-              src={`https://github.com/${username}.png`}
+              src={
+                username.length > 2 ? `https://github.com/${username}.png` : ""
+              }
             />
             <Text
               variant="body4"
